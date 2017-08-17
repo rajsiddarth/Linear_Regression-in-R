5ef567c974b8a6dcd26390e9e1738438d436bf3d
Siddarth <rajasiddartha94@gmail.com>
2017-08-17 15:13:00 -0700

    adding simple reg R code

--- simplelinreg.R
+++ simplelinreg.R
@@ -1,101 +1,67 @@
 rm(list=ls(all=TRUE))
 
 #install.packages("RCurl")
-
 library(RCurl)
 
 # Reading the data 
-data = read.table(getURL("https://raw.githubusercontent.com/rajsiddarth/Regression_in_R/master/car_price_age.csv") ,header=T,sep=",")
+data = read.table(text=getURL("https://raw.githubusercontent.com/rajsiddarth/Regression_in_R/master/car_price_age.csv") ,header=T,sep=",")
+
 names(data)
-
-# Drop the Id, Model attributes
-data = data[,-c(1,2)]
-
-# Summary
 str(data)
 summary(data)
 
 # correlation between the attributes
 cor(data)
+colnames(data)[2]="age"
 
-plot(data$Age_06_15, data$Price)
+#Dependent variable=Price, Ind variable=age
+plot(data$age, data$Price)
 
-# Linear regression model
-LinReg = lm(Price~Age_06_15, data=data)
-coefficients(LinReg)
+# Linear regression model in one variable
+lm_model = lm(formula = Price~age, data=data)
+summary(lm_model)
 
-# summary of model
-summary(LinReg)
+# plotting Confidence Intervals 
+# plotting Prediction Intervals 
+conf_int=data.frame(predict(lm_model, data, interval="confidence",level=0.95))
+pred_int=data.frame(predict(lm_model, data, interval="prediction",level=0.95))
 
-#Estimate 'Price', if 'Age_06_15' is 60
-as.numeric(coefficients(LinReg)[1] + coefficients(LinReg)[2]*60)
-
-# Using the predictions
-testdata = data.frame(Age_06_15=60)
-testdata
-predict(LinReg, testdata)
-
-#Confidence Intervals at 0.95 for Price, if 'Age_06_15' is 60
-#confidence limits value for the predicted value at 0.95 significance level
-predict(LinReg, testdata, interval="confidence",level=0.95)
-
-
-# Confidence Intervals talk about the average values intervals
-# Prediction Intervals talk about the all individual values intervals
-Pred<-data.frame(predict(LinReg, data, interval="confidence",level=0.95))
-Pred1<-data.frame(predict(LinReg, data, interval="prediction",level=0.95))
-
-names(Pred)
-
-# plot the residuals
-plot(data$Age_06_15, LinReg$residuals)
-
-resid(LinReg)
-LinReg$residuals
-
-plot(data$Age_06_15, resid(LinReg), 
-     ylab="Residuals", xlab="Age_06_15", 
-     main="Residuals plot") 
+# Residual  plot
+plot(data$age, lm_model$residuals)
+plot(data$age, resid(lm_model),ylab="Residuals", xlab="age",main="Residuals plot") 
 abline(0, 0)                  # the horizon
 
-# plot for the original data
-plot(data$Age_06_15,data$Price)
+
+
+plot(data$age,data$Price)
 # Plotting data, fitted line and confidence limits
 
-points(data$Age_06_15,Pred$fit,type="l", col="red", lwd=2)
-points(data$Age_06_15,Pred$lwr,pch="-", col="red", lwd=4)
-points(data$Age_06_15,Pred$upr,pch="-", col="red", lwd=4)
-points(data$Age_06_15,Pred1$lwr,pch="-", col="blue", lwd=4)
-points(data$Age_06_15,Pred1$upr,pch="-", col="blue", lwd=4)
-
-plot(LinReg)
+points(data$age,conf_int$fit,type="l", col="red", lwd=2)
+points(data$age,conf_int$lwr,pch="-", col="red", lwd=4)
+points(data$age,conf_int$upr,pch="-", col="red", lwd=4)
+points(data$age,pred_int$lwr,pch="-", col="blue", lwd=4)
+points(data$age,pred_int$upr,pch="-", col="blue", lwd=4)
+plot(lm_model)
 
 # Error metrics evaluation on train data and test data
 
-########################################################
-
-rm(list=ls(all=TRUE))
-data<-read.csv("Toyota_SimpleReg.csv",header=T)
-names(data)
-# drop the Id, Model attributes
-data = data[,-c(1,2)]
-str(data)
-summary(data)
-
-
 #Split the data into train and test data sets
 rows=seq(1,nrow(data),1)
-set.seed(123)
+set.seed(50)
 trainRows=sample(rows,(70*nrow(data))/100)
 train = data[trainRows,] 
 test = data[-trainRows,]
 
-# Linear regression model
-LinReg1<-lm(Price~Age_06_15,data=train)
-summary(LinReg1)
+# Linear regression model on train data 
+lm_train=lm(Price~age,data=train)
+summary(lm_train)
+#
+install.packages("DMwR")
 library(DMwR)
+
 #Error verification on train data
-regr.eval(train$Price, LinReg1$fitted.values) 
+regr.eval(train$Price, lm_train$fitted.values) 
+
 #Error verification on test data
-Pred<-predict(LinReg1,test)
-regr.eval(test$Price, Pred)
+pred_test=predict(lm_train,test)
+regr.eval(test$Price, pred_test)
