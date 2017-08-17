e3e9a852481e2780661d93f70ca1da055b952455
Siddarth <rajasiddartha94@gmail.com>
2017-08-17 15:41:44 -0700

    update logistic code

--- Logistic_reg.R
+++ Logistic_reg.R
@@ -1,79 +1,52 @@
 
-# LOGISTIC REGRESSION
+rm(list=ls(all=T))
+library(RCurl)
 
-# Read in Flier Response Data
-flierresponse <- read.csv("FlierResponse.csv", header = T, sep = ",")
-flierresponse
-str(flierresponse)
-flierresponse$Response <- as.factor(flierresponse$Response)
-str(flierresponse)
-flierresponseglm <- glm(Response~Age, data = flierresponse, family = "binomial")
-flierresponseglm
-summary(flierresponseglm)
-logLik(flierresponseglm)
-deviance(flierresponseglm)
-AIC(flierresponseglm)
+data=read.table(text=getURL("https://raw.githubusercontent.com/rajsiddarth/Regression_in_R/master/FlierResponse.csv"),header = T,sep = ",")
 
-flierresponseglm <- glm(Response~1, data = flierresponse, family = "binomial")
-flierresponseglm
-summary(flierresponseglm)
+str(data)
 
-# CASE STUDY - The Framingham Heart Study
+#Converting response to factors
+data$Response = as.factor(data$Response)
 
-# Read in the Framingham dataset
-framingham = read.csv("framingham.csv")
+#Building Logistic model
 
-# Look at structure
-str(framingham)
+model_glm= glm(Response~Age, data = data, family = "binomial")
+model_glm
 
-# Load the library caTools
+summary(model_glm)
+
 library(caTools)
 
 # Randomly split the data into training and testing sets
-set.seed(1000)
-split = sample.split(framingham$TenYearCHD, SplitRatio = 0.70)
+set.seed(123)
+split = sample.split(data$Response, SplitRatio = 0.70)
 
 # Split up the data using subset
-train = subset(framingham, split==TRUE)
-test = subset(framingham, split==FALSE)
+train = subset(data, split==TRUE)
+test = subset(data, split==FALSE)
 
-# Logistic Regression Model
-framinghamLog = glm(TenYearCHD ~ ., data = train, family=binomial)
-summary(framinghamLog)
+#Model on train
+model_train= glm(Response~., data = train, family = "binomial")
 
 #Accuracy on the training set
-predictTrain = predict(framinghamLog, type="response", newdata=train)
+predict_train = predict(model_train, type="response", newdata=train)
 
 # Confusion matrix with threshold of 0.5
-table(train$TenYearCHD, predictTrain > 0.5)
-
-# Accuracy on Train Set
-(2170+30)/(2170+30+357+9)
+table(train$Response, predict_train > 0.5)
 
 # Predictions on the test set
-predictTest = predict(framinghamLog, type="response", newdata=test)
+predict_test = predict(model_train, type="response", newdata=test)
 
 # Confusion matrix with threshold of 0.5
-table(test$TenYearCHD, predictTest > 0.5)
+table(test$Response, predict_test > 0.5)
 
-# Accuracy on Test Set
-(915+12)/(915+12+158+7)
-
-# Confusion matrix with threshold of 0.9
-table(test$TenYearCHD, predictTest > 0.9)
-# Confusion matrix with threshold of 0.7
-table(test$TenYearCHD, predictTest > 0.7)
-# Confusion matrix with threshold of 0.5
-table(test$TenYearCHD, predictTest > 0.5)
-# Confusion matrix with threshold of 0.3
-table(test$TenYearCHD, predictTest > 0.3)
-# Confusion matrix with threshold of 0.1
-table(test$TenYearCHD, predictTest > 0.1)
-
-# Test set AUC 
+# AUC 
+install.packages("ROCR")
 library(ROCR)
-ROCRpred = prediction(predictTest, test$TenYearCHD)
-as.numeric(performance(ROCRpred, "auc")@y.values)
-ROCRperf <- performance(ROCRpred, "tpr", "fpr")
+
+ROCR_pred = prediction(predict_test, test$Response)
+as.numeric(performance(ROCR_pred, "auc")@y.values)
+ROCRperf=performance(ROCR_pred, "tpr", "fpr")
 par(mfrow=c(1,1))
 plot(ROCRperf, colorize = TRUE, print.cutoffs.at=seq(0,1,by=0.1), text.adj=c(-0.2,1.7))
